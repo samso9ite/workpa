15eb3f1d08776f842e4e43252c3e0db08d699f58
Samson Ajayi <samso9ite@gmail.com>
2023-10-18 21:37:57 +0100

    Next Env Fix

--- next.config.js
+++ next.config.js
@@ -5,18 +5,3 @@ const nextConfig = {
 
 module.exports = nextConfig
 
-const webpack = require('webpack');
-
-module.exports = {
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-
-        config.plugins.push(new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery',
-            'window.jQuery': 'jquery'
-        }))
-        
-        // Important: return the modified config
-        return config;
-    }
-}
